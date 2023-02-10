038d8ffdacc17235ae7ba89f730f522407a80937
moshiur rahman autul <autul2017831021>
2023-02-10 20:47:37 +0600

    mp3 to wav converter updated

--- Music Downloader & Converter/mp3-to-wav-converter.py
+++ Music Downloader & Converter/mp3-to-wav-converter.py
@@ -1,8 +1,9 @@
 import subprocess
 import glob
 mp3_path = 'mp3\\'
-
+wav_path = 'wav\\'
 for audio in glob.glob(mp3_path + '*'):
     song_name = audio.split('\\')[-1]
     wav_song_name = song_name.split('.')[-2] + '.wav'
-    subprocess.call(['ffmpeg', '-i', audio, wav_song_name])
+    wav_song = wav_path+wav_song_name
+    subprocess.call(['ffmpeg', '-i', audio, wav_song])
