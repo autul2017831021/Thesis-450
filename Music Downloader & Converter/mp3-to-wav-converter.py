94af03863ac811f8627fa73eea247d7fdc3aedae
moshiur rahman autul <autul2017831021>
2023-02-13 04:29:18 +0600

    updated

--- Music Downloader & Converter/mp3-to-wav-converter.py
+++ Music Downloader & Converter/mp3-to-wav-converter.py
@@ -1,7 +1,9 @@
 import subprocess
 import glob
 mp3_path = 'mp3\\'
-wav_path = 'wav\\'
+bangladesh_song_folder = 'bangladesh_song_folder\\'
+kolkata_song_folder = 'kolkata_song_folder\\'
+wav_path = kolkata_song_folder
 for audio in glob.glob(mp3_path + '*.mp3'):
     song_name = audio.split('\\')[-1]
     wav_song_name = song_name.split('.')[-2] + '.wav'
